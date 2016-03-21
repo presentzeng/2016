edb9c1f03a61e99fcaaf5ea0edc3d04a33f600bc
presentzeng <presentzeng@gmail.com>
2016-03-22 04:23:34 +0800

    auto commit date is 2016-03-22-04:23:34

--- sicp/1.31.scm
+++ sicp/1.31.scm
@@ -1,4 +1,13 @@
 (define func (lambda (x) (+ x 4)))
-  
 
-(func 2)
+; (a, b) func(a) * func(a+1) .... * func(b)
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product term (next a) next b))))
+
+(product (lambda (x) x)
+	 1
+	 (lambda (x) (+ x 1))
+	 10)
+
