3b8404fdf6943763018302048a07ecd4109fc332
presentzeng <presentzeng@gmail.com>
2016-07-26 16:56:16 +0800

    daily practice

--- sicp/2.32_subset.scm
+++ sicp/2.32_subset.scm
@@ -5,11 +5,12 @@
   ;    list
    ;    list))
 
+;;flag
 (define (subset s)
   (if (null? s)
       (list ())
       (let ((rest (subset (cdr s))))
-	(append rest (map subset rest)))))
+	(append rest (map (lambda (x) (cons (car s) x)) rest)))))
 
 
 
---
+++ sicp/2.33_basic_operation.scm
@@ -0,0 +1,42 @@
+;;flag
+(define (accumulate op init list)
+  (if (null? list)
+      init
+      (op 
+       (car list)
+       (accumulate op init (cdr list)))))
+
+
+;;use accumulate to finish some basic operations
+
+(define (map1 proc list)
+  (accumulate (lambda (x y) 
+		(cons (proc x) y))
+	      () list))
+
+
+(define (append1 se1 se2)
+  (accumulate cons se2 se1))
+	      
+;;;wrong example
+
+;(define (length list)
+;  (accumulate (lambda (x y)
+;		(cond ((null? y) 0)
+;		      ((not (pair y)) 1)
+;		      (else
+;		       (+ x (length (cdr y))))))
+;		0 list))
+
+   
+(define (length1 list)
+  (accumulate (lambda (x y)
+		(+ 1 y))
+	      0 list))
+		
+(define one (list 1 2 3))
+(define two (list 5 6 7))
+
+(length1 one)
+;(append1 one two)
+;(map1 square one)    		   
